5ac344c55f00e1bf09c0ac41eb698f91eeec86ed
Paul S <maus@paulstrobach.de>
2020-01-04 23:20:24 +0100

    Stylistic improvements to docs.js See styleguide: https://github.com/codidact/core/wiki/Code-standards:-JS

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -4,20 +4,23 @@
 // --- not for production ---
 // --------------------------
 
-hs_triggers = document.querySelectorAll("[data-trigger-header-slide]");
+/* Header slides */
+const headerSlideTriggers = document.querySelectorAll("[data-trigger-header-slide]");
 
-for(var i=0; i < hs_triggers.length; i++) {
-  hs_triggers[i].addEventListener("click", function(e) {
-    hs = document.querySelector(this.getAttribute("data-trigger-header-slide"));
+for (const i = 0; i < headerSlideTriggers.length; i++) {
+  headerSlideTriggers[i].addEventListener("click", function (e) {
+    const headerSlide = document.querySelector(this.getAttribute("data-trigger-header-slide"));
 
-    hs.classList.toggle("is-active");
+    headerSlide.classList.toggle("is-active");
     this.classList.toggle("is-active");
 
-    rect = this.getBoundingClientRect();
-
+    // Position header slide appropriately relative to
+    // trigger.
+    const rect = this.getBoundingClientRect();
     hs.style.top = (rect.top + rect.height) + "px";
     hs.style.right = (document.body.clientWidth - rect.right) + "px";
 
+    // Prevent navigation
     e.preventDefault();
   });
 }
