50c8227a8397de5942c8a414095e6019c1e1d71e
Paul S <maus@paulstrobach.de>
2020-01-04 23:17:01 +0100

    Stylistic improvements to docs.js Apply style guide: https://github.com/codidact/core/wiki/Code-standards:-JS

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -1,16 +1,22 @@
-m_triggers = document.querySelectorAll("[data-trigger-modal]");
+/* Modals */
+const modalTriggers = document.querySelectorAll("[data-trigger-modal]");
 
-for(var i=0; i < m_triggers.length; i++) {
-  mt = m_triggers[i];
+for (const i = 0; i < modalTriggers.length; i++) {
+  modalTrigger = modalTriggers[i];
 
-  mt.addEventListener("click", function(e) {
+  modalTrigger.addEventListener("click", function (e) {
     modal = document.querySelector(this.getAttribute("data-trigger-modal"));
 
-    if(!modal.classList.contains("is-with-backdrop"))
+    // Set appropriate on-screen position for modals,
+    // which do not have .is-with-backdrop, and are
+    // therefore position absolute not fixed.
+    if(!modal.classList.contains("is-with-backdrop")) {
       modal.style.top = document.scrollingElement.scrollTop + "px";
+    }
 
     modal.classList.toggle("is-active");
 
+    // Prevent navigation
     e.preventDefault();
-  })
+  });
 }
